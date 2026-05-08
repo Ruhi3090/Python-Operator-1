c02a48047c46e328a8039490493034900b3977bf
Ruhi3090 <rm1204.kig@ked.edu.in>
2026-05-08 18:23:42 +0530

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,12 @@
+English=2
+Math=87
+Science=23
+Gp=49
+Computing=100
+German=65
+#Find Sum of all marks
+sum=English+Math+Science+Gp+German+Computing
+print("The sum of all the marks is :",sum)
+#Find Average
+Avg=sum/6
+print("The average of all marks is:",Avg)
